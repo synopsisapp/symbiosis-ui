104cc7ec1f8d8bfe83b9c63b52ff63fc0e872679
DGiannaris <mtsgiannaris@gmail.com>
2024-09-05 16:17:07 +0300

    chore: Add shadcn charts groundwork

---
+++ packages/ui/src/components/Charts/types.ts
@@ -0,0 +1,10 @@
+import type { THEMES } from "./constants";
+
+export type ChartConfig = {
+  [k in string]: {
+    label?: React.ReactNode;
+    icon?: React.ComponentType;
+  } & ({ color?: string; theme?: never } | { color?: never; theme: Record<keyof typeof THEMES, string> });
+};
+
+export type * from "recharts";
