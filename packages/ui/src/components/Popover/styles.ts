63065678f3ec706136828bc77c4750f9428d4922
Dimitris Giannaris <mtsgiannaris@gmail.com>
2024-09-17 18:13:14 +0300

    Introduce Popover component (#2)
    * chore: rename classname prop in IconButton
    
    * chore: Add @radix-ui/react-popover dependency
    
    * feature: Introduce Popover component
    
    * chore: Add Popover component storybook stories
    
    * chore: housekeeping
    
    * chore: format files

---
+++ packages/ui/src/components/Popover/styles.ts
@@ -0,0 +1,10 @@
+import { cn } from "../../utils/cn";
+
+export const sharedPopoverStyles = cn(
+  "p-5 rounded-md bg-white relative shadow-lg",
+  "will-change-[transform,opacity]",
+  "data-[state=delayed-open]:data-[side=top]:animate-slide-up-and-fade",
+  "data-[state=delayed-open]:data-[side=right]:animate-slide-right-and-fade",
+  "data-[state=delayed-open]:data-[side=left]:animate-slide-left-and-fade",
+  "data-[state=delayed-open]:data-[side=bottom]:animate-slide-down-and-fade",
+);
---
+++ packages/ui/src/components/Popover/types.ts
@@ -0,0 +1,36 @@
+import type {
+  PopoverArrowProps as RadixPopoverArrowProps,
+  PopoverContentProps as RadixPopoverContentProps,
+} from "@radix-ui/react-popover";
+import type { IconProps } from "../Icon/types";
+import type { ButtonTone } from "../Button/types";
+
+export type PopoverRootProps = {
+  children: React.ReactNode;
+  defaultOpen?: boolean;
+  open?: boolean;
+  onOpenChange?: (open: boolean) => void;
+};
+
+export type PopoverContentProps = {
+  children?: React.ReactNode;
+  side?: RadixPopoverContentProps["side"];
+  className?: string;
+  closeIcon?: IconProps["name"];
+  tone?: ButtonTone;
+};
+
+export type PopoverArrowProps = RadixPopoverArrowProps & {
+  className?: string;
+};
+
+export type PopoverTriggerProps = {
+  children: React.ReactNode;
+};
+
+export type PopoverProps = {
+  Root: PopoverRootProps;
+  Content: PopoverContentProps;
+  Trigger: PopoverTriggerProps;
+  Arrow: PopoverArrowProps;
+};
