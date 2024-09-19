325bf394cceae5cb99a490abb4a4adea43033d77
Dimitris Giannaris <mtsgiannaris@gmail.com>
2024-09-19 11:06:36 +0300

    chore: switch to use correct identation (#12)

--- packages/ui/src/components/Switch/types.ts
+++ packages/ui/src/components/Switch/types.ts
@@ -1,13 +1,13 @@
 import type { Sizes } from "../../designSystemTokens";
 
 export type SwitchProps = {
-	defaultChecked?: boolean;
-	onCheckedChange?: (checked: boolean) => void;
-	label?: string;
-	size?: Sizes;
-	value?: boolean;
-	name?: string;
-	id?: string;
-	disabled?: boolean;
-	className?: string;
+  defaultChecked?: boolean;
+  onCheckedChange?: (checked: boolean) => void;
+  label?: string;
+  size?: Sizes;
+  value?: boolean;
+  name?: string;
+  id?: string;
+  disabled?: boolean;
+  className?: string;
 };
