46274c07cf2d0f12de03d28819bcad07263aa6fc
DGiannaris <mtsgiannaris@gmail.com>
2024-09-19 18:18:02 +0300

    chore: add required table icons

--- packages/ui/src/components/Icon/iconTypes.d.ts
+++ packages/ui/src/components/Icon/iconTypes.d.ts
@@ -3,6 +3,10 @@ declare namespace SymbiosisUI {
     | "symbiosis-chevron-down"
     | "symbiosis-minus"
     | "symbiosis-check"
+    | "symbiosis-kebab"
+    | "symbiosis-sort-rows-asc"
+    | "symbiosis-sort-rows-desc"
+    | "symbiosis-select"
     | "symbiosis-dots"
     | "symbiosis-chevron-left"
     | "symbiosis-chevron-right";
