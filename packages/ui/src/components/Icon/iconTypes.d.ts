a647491d123a091c34d6a2d013f2dbcb60e125ef
DGiannaris <mtsgiannaris@gmail.com>
2024-09-23 18:03:38 +0300

    chore: add internal icon

--- packages/ui/src/components/Icon/iconTypes.d.ts
+++ packages/ui/src/components/Icon/iconTypes.d.ts
@@ -9,5 +9,10 @@ declare namespace SymbiosisUI {
     | "symbiosis-select"
     | "symbiosis-dots"
     | "symbiosis-chevron-left"
-    | "symbiosis-chevron-right";
+    | "symbiosis-chevron-right"
+    | "symbiosis-chevron-down"
+    | "symbiosis-minus"
+    | "symbiosis-check"
+    | "symbiosis-exclamation-circle"
+    | "symbiosis-help-circle";
 }
