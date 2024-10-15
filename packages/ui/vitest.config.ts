fbdc5e06d8fe5a11989e2422506d6597a7d42c5d
DGiannaris <mtsgiannaris@gmail.com>
2024-10-15 13:58:12 +0300

    text: remove vitest globals and related files

--- packages/ui/vitest.config.ts
+++ packages/ui/vitest.config.ts
@@ -5,8 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   test: {
-    globals: true,
     environment: "jsdom",
-    setupFiles: ["./src/test/setup.ts"],
   },
 });
