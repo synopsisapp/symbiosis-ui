b38696aed365b68b380664fb1389893726ba2c54
Anatolie Sernii <anatoliesernii@gmail.com>
2024-12-06 12:17:43 +0200

    fix: overlapping issues with z-index on DataTables and DropDowns. Adds Story to showcase pinned header and DropDown are not overlapping

--- examples/storybook/src/stories/DataTable.stories.tsx
+++ examples/storybook/src/stories/DataTable.stories.tsx
@@ -274,6 +274,27 @@ export const WithPinningAndStickyHeader: Story = {
   ],
 };
 
+export const WithBulkActionsAndStickyHeader: Story = {
+  args: {
+    ...Selectable.args,
+    stickyHeader: true,
+    quickActionsText: "Bulk Actions",
+    headerActions: [
+      {
+        text: "Delete Selected",
+        icon: "symbiosis-minus",
+        onSelect: (rows) => console.log("Delete selected rows:", rows),
+      },
+      {
+        text: "Export Selected",
+        icon: "google-play",
+        onSelect: (rows) => console.log("Export selected rows:", rows),
+      },
+    ],
+    className: "h-[300px]",
+  },
+};
+
 export const CustomNoResultsText: Story = {
   args: {
     ...Default.args,
