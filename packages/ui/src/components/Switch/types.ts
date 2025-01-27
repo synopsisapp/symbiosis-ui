eb0fce4f65310d2cb88d5d5f22db87a93a940520
Alex Bratsos <alexisbratsos@gmail.com>
2025-01-27 21:10:03 +0800

    feat: add content alignment option on Switch

--- packages/ui/src/components/Switch/types.ts
+++ packages/ui/src/components/Switch/types.ts
@@ -1,13 +1,26 @@
-import type { Sizes } from "../../designSystemTokens";
+import z from "zod";
+import { Sizes } from "../../designSystemTokens";
 
-export type SwitchProps = {
-  defaultChecked?: boolean;
-  onCheckedChange?: (checked: boolean) => void;
-  label?: string;
-  size?: Sizes;
-  value?: boolean;
-  name?: string;
-  id?: string;
-  disabled?: boolean;
-  className?: string;
-};
+export const SwitchSizes = z.enum([
+  Sizes.Enum["small-200"],
+  Sizes.Enum["small-100"],
+  Sizes.Enum.base,
+  Sizes.Enum["large-100"],
+]);
+
+export const SwitchContentAlignment = z.enum(["label-first", "switch-first"]);
+
+export const SwitchProps = z.object({
+  defaultChecked: z.boolean().optional(),
+  onCheckedChange: z.function().args(z.boolean()).returns(z.void()).optional(),
+  label: z.string().optional(),
+  size: SwitchSizes.optional(),
+  value: z.boolean().optional(),
+  name: z.string().optional(),
+  id: z.string().optional(),
+  disabled: z.boolean().optional(),
+  className: z.string().optional(),
+  contentAlignment: SwitchContentAlignment.optional(),
+});
+
+export type SwitchProps = z.infer<typeof SwitchProps>;
